c9b26fdcd744fdbc332c544779b8e31146213fe7
kaeteyaruyo <kaeteyaruyo@gmail.com>
2019-11-21 04:17:23 +0800

    Add bit array
    * We used bit table in bloom filter in byte. It wasted 7 / 8 of space.
    * To maximize space utilization, we need to treat table as a bit array
      and use each slot of it. So we add two macros to do that:
        * set_bit(table, index): set index'th bit in table
        * test_bit(table, index): test if index'th bit in table is set

--- bloom.c
+++ bloom.c
@@ -1,6 +1,8 @@
 #include "bloom.h"
 #include "stdint.h"
 #include "stdlib.h"
+#define set_bit(table, index) (table[(index / 8)] |= (1 << (index % 8)))
+#define test_bit(table, index) (table[(index / 8)] & (1 << (index % 8)))
 
 struct bloom_hash {
     hash_function func;
@@ -10,7 +12,7 @@ struct bloom_hash {
 struct bloom_filter {
     struct bloom_hash *func;
     void *bits;
-    size_t size;
+    size_t size;  // In bits
 };
 
 static unsigned int djb2(const void *_str)
@@ -43,8 +45,9 @@ static unsigned int jenkins(const void *_str)
 bloom_t bloom_create(size_t size)
 {
     bloom_t res = calloc(1, sizeof(struct bloom_filter));
-    res->size = size;
-    res->bits = calloc(size, 1);
+    res->size = size;  // Store size in bit
+    res->bits =
+        calloc(size >> 3, 1);  // But alloc memory in byte, with 1 / 8 size
 
     bloom_add_hash(res, djb2);
     bloom_add_hash(res, jenkins);
@@ -83,11 +86,11 @@ void bloom_add_hash(bloom_t filter, hash_function func)
 void bloom_add(bloom_t filter, const void *item)
 {
     struct bloom_hash *h = filter->func;
-    uint8_t *bits = filter->bits;
+    uint8_t *bits = filter->bits;  // Convert from void* to uint8_t*
     while (h) {
-        unsigned int hash = h->func(item);
-        hash %= filter->size;
-        bits[hash] = 1;
+        unsigned hash = h->func(item);
+        hash %= filter->size;  // Index in bit
+        set_bit(bits, hash);
         h = h->next;
     }
 }
@@ -99,7 +102,7 @@ bool bloom_test(bloom_t filter, const void *item)
     while (h) {
         unsigned int hash = h->func(item);
         hash %= filter->size;
-        if (!(bits[hash])) {
+        if (!test_bit(bits, hash)) {
             return false;
         }
         h = h->next;
